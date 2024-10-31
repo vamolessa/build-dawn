d3791a92165a4d59edf9b2e5b3d5ce2c5b89cb10
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2024-10-31 10:48:05 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,5 +20,128 @@ else
   exit 1
 fi
 
+#
+# dependencies
+#
+
+which git > /dev/null || (
+  echo "ERROR: git not found"
+  exit 1
+)
+
+which cmake > /dev/null || (
+  echo "ERROR: cmake not found"
+  exit 1
+)
+
+#
+# 7-Zip
+#
+
+#if exist "%ProgramFiles%\7-Zip\7z.exe" (
+#  set SZIP="%ProgramFiles%\7-Zip\7z.exe"
+#) else (
+#  where /q 7za.exe || (
+#    echo ERROR: 7-Zip installation or "7za.exe" not found
+#    exit /b 1
+#  )
+#  set SZIP=7za.exe
+#)
+
+#
+# get depot tools
+#
+
+PATH=$PWD/depot_tools:$PATH
+DEPOT_TOOLS_MAC_TOOLCHAIN=0
+
+if [ ! -d depot_tools ]; then
+  git clone --depth=1 --no-tags --single-branch https://chromium.googlesource.com/chromium/tools/depot_tools.git || exit 1
+fi
+
+#
+# clone dawn
+#
+
+if ["$DAWN_COMMIT" == ""]; then
+  DAWN_COMMIT=$(git ls-remote https://dawn.googlesource.com/dawn HEAD | cut -w -f 1)
+fi
+
+if [ ! -d dawn ]; then
+  mkdir dawn
+  pushd dawn
+  git init . || exit 1
+  git remote add origin https://dawn.googlesource.com/dawn || exit 1
+  popd
+fi
+
+pushd dawn
+
+git fetch origin $DAWN_COMMIT || exit 1
+git checkout --force FETCH_HEAD || exit 1
+
+cp -f scripts\standalone.gclient .gclient
+sed -i.bak -e "/'third_party\/catapult'\: /,+3d" -e "/'third_party\/swiftshader'\: /,+3d" -e "/'third_party\/angle'\: /,+3d" -e "/'third_party\/webgpu-cts'\: /,+3d" -e "/'third_party\/vulkan-validation-layers\/src'\: /,+3d" -e "/'third_party\/khronos\/OpenGL-Registry'\: /,+3d" DEPS || exit 1
+gclient sync -f -D -R || exit 1
+
+popd
+
+#
+# build dawn
+#
+
+cmake                                         \
+  -S dawn                                     \
+  -B dawn.build-$ARCH                         \
+  -A $ARCH                                    \
+  -D CMAKE_BUILD_TYPE=Release                 \
+  -D CMAKE_POLICY_DEFAULT_CMP0091=NEW         \
+  -D CMAKE_POLICY_DEFAULT_CMP0092=NEW         \
+  -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded \
+  -D BUILD_SHARED_LIBS=OFF                    \
+  -D BUILD_SAMPLES=OFF                        \
+  -D DAWN_ENABLE_METAL=ON                     \
+  -D DAWN_ENABLE_NULL=OFF                     \
+  -D DAWN_ENABLE_DESKTOP_GL=OFF               \
+  -D DAWN_ENABLE_OPENGLES=OFF                 \
+  -D DAWN_ENABLE_VULKAN=ON                    \
+  -D DAWN_USE_GLFW=OFF                        \
+  -D DAWN_BUILD_SAMPLES=OFF                   \
+  -D TINT_ENABLE_INSTALL=ON                   \
+  -D TINT_BUILD_SPV_READER=ON                 \
+  -D TINT_BUILD_WGSL_WRITER=ON                \
+  -D TINT_BUILD_TESTS=OFF                     \
+  || exit 1
+
+cmake --build dawn.build-$ARCH --config Release --target webgpu_dawn tint_cmd_tint_cmd --parallel || exit 1
+
+#
+# prepare output folder
+#
+
+mkdir dawn-$ARCH
+
+echo $DAWN_COMMIT > dawn-$ARCH/commit.txt
+
+cp -f dawn.build-$ARCH/gen/include/dawn/webgpu.h              dawn-$ARCH
+cp -f dawn.build-$ARCH/Release/webgpu_dawn.so                 dawn-$ARCH
+cp -f dawn.build-$ARCH/Release/tint                           dawn-$ARCH
+cp -f dawn.build-$ARCH/src/dawn/native/Release/webgpu_dawn.so dawn-$ARCH
+
+#
+# Done!
+#
+
+if [ ! -z "$GITHUB_WORKFLOW" ]; then
+
+  #
+  # GitHub actions stuff
+  #
+
+  $SZIP a -y -mx=9 dawn-win-%ARCH%-%BUILD_DATE%.zip dawn-$ARCH || exit 1
+fi
+
+###
+
 echo "ARCH = $ARCH !!"
-echo "placeholder" > dawn-mac-${PROCESSOR_ARCHITECTURE}-${BUILD_DATE}.zip
+echo "placeholder" > dawn-mac-${ARCH}-${BUILD_DATE}.zip
