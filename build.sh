5d68efcaab9505c7b23465b55a44f1b8a39d99e8
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2024-10-31 09:49:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,24 @@
-PROCESSOR_ARCHITECTURE=$1
+cd "$(dirname "$0")"
+
+#
+# build architecture
+#
+
+if [ "$1" == "x64" ]; then
+  ARCH=x64
+elif [ "$1" == "arm64" ]; then
+  ARCH=arm64
+elif [ -z "$1"]; then
+  echo "Unknown target '$1' architecture!"
+  exit 1
+elif [ "$PROCESSOR_ARCHITECTURE" == "AMD64" ]; then
+  ARCH=x64
+elif [ "$PROCESSOR_ARCHITECTURE" == "ARM64" ]; then
+  ARCH=arm64
+else
+  echo "Unknown target architecture!"
+  exit 1
+fi
+
+echo "ARCH = $ARCH !!"
 echo "placeholder" > dawn-mac-${PROCESSOR_ARCHITECTURE}-${BUILD_DATE}.zip
