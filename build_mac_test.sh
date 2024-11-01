140f559eaa9acd8cb187e6fa01e50bc8f98d5a14
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2024-11-01 21:47:27 +0000

    Update build_mac_test.sh

--- build_mac_test.sh
+++ build_mac_test.sh
@@ -1,11 +1,8 @@
-# Check XCode version
-ls `xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs
-
 # Clone the repo as "dawn"
 git clone https://dawn.googlesource.com/dawn dawn && cd dawn
 
 # Fetch dependencies (lose equivalent of gclient sync)
-python tools/fetch_dawn_dependencies.py --use-test-deps
+python tools/fetch_dawn_dependencies.py
 
 mkdir -p out/Debug
 cd out/Debug
@@ -14,6 +11,7 @@ make # -j N for N-way parallel build
 
 ls -R
 
+# Zip build output
 zip -9 -d -r dawn-mac-$ARCH-$BUILD_DATE.zip .
 cp dawn-mac-$ARCH-$BUILD_DATE.zip ../..
 
