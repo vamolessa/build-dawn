2e882bcce04286942b3d33a2462f8c0f3d1996e0
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2025-03-28 12:36:45 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ cmake                                         \
   -D DAWN_ENABLE_METAL=ON                     \
   -D DAWN_ENABLE_NULL=OFF                     \
   -D DAWN_ENABLE_DESKTOP_GL=OFF               \
-  -D DAWN_ENABLE_OPENGLES=OFF                 \
+  -D DAWN_ENABLE_OPENGLES=ON                  \
   -D DAWN_ENABLE_VULKAN=ON                    \
   -D DAWN_USE_GLFW=OFF                        \
   -D DAWN_ENABLE_SPIRV_VALIDATION=ON          \
@@ -29,6 +29,7 @@ cmake                                         \
   -D TINT_ENABLE_INSTALL=ON                   \
   -D TINT_BUILD_SPV_READER=ON                 \
   -D TINT_BUILD_WGSL_WRITER=ON                \
+  -D TINT_BUILD_GLSL_WRITER=ON                \
   -D TINT_BUILD_TESTS=OFF                     \
   || exit 1
 
